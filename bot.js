366cf6d6770e605cdecc5584de1144717409ac99
DefaultDiscord <Brazier.Horton@hotmail.co.uk>
2019-08-14 18:21:38 +0100

    logging function updated 2

--- bot.js
+++ bot.js
@@ -35,25 +35,9 @@ client.on('error', function(err) {
 
 
 // log deleted messages
-client.on('messageDelete', async (message) => {
-  const logs = message.guild.channels.find(channel => channel.name === "sentinel");
-  if (message.guild.me.hasPermission('MANAGE_CHANNELS') && !logs) {
-    message.guild.createChannel('sentinel', 'text');
-  }
-  if (!message.guild.me.hasPermission('MANAGE_CHANNELS') && !logs) {
-    console.log('The logs channel does not exist and tried to create the channel but I am lacking permissions')
-  }
-  const entry = await message.guild.fetchAuditLogs({type: 'MESSAGE_DELETE'}).then(audit => audit.entries.first())
-  let user = ""
-    if (entry.extra.channel.id === message.channel.id
-      && (entry.target.id === message.author.id)
-      && (entry.createdTimestamp > (Date.now() - 5000))
-      && (entry.extra.count >= 1)) {
-    user = entry.executor.username
-  } else {
-    user = message.author.username
-  }
-  logs.send(`Message deleted in ${message.channel.name} in server ${helpmelol} by ${user}`);
+client.on("messageDelete", (messageDelete) => {
+ client.channels.get("611243996521431135").send (`**Message : "${messageDelete.content}" by ${messageDelete.author.tag}
+ was deleted in channel ${message.channel.name} on server ${guild.name} at ${new Date()}.**`)
 });
 
 client.on("message", (message) => {
