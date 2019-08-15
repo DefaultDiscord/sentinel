272b1f097d4dca9283a1244b17f73f7054d297c4
DefaultDiscord <Brazier.Horton@hotmail.co.uk>
2019-08-15 15:54:47 +0100

    Log now shows user Avatar

--- bot.js
+++ bot.js
@@ -52,7 +52,7 @@ client.on("messageDelete", (message) => {
       "text": "sentinel logging"
     },
     "thumbnail": {
-      "url": "https://i.imgur.com/dyb2MdQ.png"
+      "url": `${message.author.avatarURL}`
     },
     "fields": [
       {
@@ -91,7 +91,7 @@ client.on("messageUpdate", (oldMessage, newMessage) => {
       "text": "sentinel logging"
     },
     "thumbnail": {
-      "url": "https://i.imgur.com/dyb2MdQ.png"
+      "url": `${message.author.avatarURL}`
     },
     "fields": [
       {
