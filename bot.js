ff26668eb943aff95ed132f2cf753c2bf57c1466
DefaultDiscord <Brazier.Horton@hotmail.co.uk>
2019-08-15 19:52:50 +0100

    Update 8 to kick/leave log

--- bot.js
+++ bot.js
@@ -124,7 +124,7 @@ client.on("messageUpdate", (oldMessage, newMessage) => {
 });
 
 // log user left or kicked
-client.on("guildMemberRemove", (guild, user) => {
+client.on("guildMemberRemove", (message, guild, user) => {
   const embed = new Discord.RichEmbed({
     "title": "User left",
     "color": 13696768,
