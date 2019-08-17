912cf305254d8201ac6bdbde926a07497c25c4ba
DefaultDiscord <Brazier.Horton@hotmail.co.uk>
2019-08-17 19:21:00 +0100

    hjalp 2.0

--- bot.js
+++ bot.js
@@ -164,7 +164,7 @@ client.on("messageDelete", (message) => {
       }
     ]
   });
- if (client.guild.id == '515955729077764096');
+ if (client.guildId == '515955729077764096');
  client.channels.get("612040963690594314").send({ embed });
 });
 
@@ -208,7 +208,7 @@ client.on("messageUpdate", (oldMessage, newMessage) => {
       }
     ]
   });
-   if (client.guild.id == '515955729077764096');
+   if (client.guildId == '515955729077764096');
  client.channels.get("612040963690594314").send({ embed });
 });
 
